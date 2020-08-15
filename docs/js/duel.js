039e2740367b290b62712eb26c916e49a2938b48
Jared Flores <jared_flores@cs.ucsb.edu>
2020-08-15 02:25:48 -0700

    houston, we have made contact

--- docs/js/duel.js
+++ docs/js/duel.js
@@ -1,13 +1,28 @@
 // duel.js
-var URL = document.location.href;
+var URL = document.location.href,
+	RTC_CFG = { 
+		iceServers : [ { url : "stun:stun2.1.google.com:19302" } ] 
+	},
+	RTC_OPT = {
+		optional : [ { RtpDataChannels : true } ]
+	};
 
 var	room_code = "",
+	duel = "",
+	game_rtc = null,
+	game_channel = null,
+	rtc_id = 0,
+	last_id = -1,
+	viewer_map = {},
 	q_found = false,
+	one = true,
+	me = "",
 	i;
 
 for( i = 0; i < URL.length; ++i ) {
 	if( q_found ) {
-		if( room_code == "room=" ) {
+		room_code = room_code.toUpperCase();
+		if( room_code == "ROOM=" ) {
 			room_code = "";
 		}
 		room_code += URL.charAt( i );
@@ -17,5 +32,134 @@ for( i = 0; i < URL.length; ++i ) {
 		}
 	}
 }
+
+duel = "/duels/" + room_code;
+
+function callback_empty( err ) {
+	if( err ) {
+		console.log( "error" );
+	}
+}
+
+function callback_rtc( snapshot ) {
+	var action = snapshot.val();
+	console.log( action );
+	if( action.from == me ) return;
+	var to;
+	if( action.from == "1" || action.from == "2" ) {
+		if( action.id <= last_id ) return;
+		last_id = action.id;
+	}
+	switch( action.type ) {
+		case "join":
+			if( action.from == "2" ) {
+				console.log( "we made it?" );
+				game_rtc.createOffer().then( function( offer ) {
+					game_rtc.setLocalDescription( offer );
+				} ).then( function() {
+					var data = {};
+					data[ "type" ] = "offer";
+					data[ "from" ] = me;
+					data[ "offer" ] = JSON.stringify( game_rtc.localDescription );
+					data[ "id" ] = rtc_id++;
+					firebase.database().ref( duel + "/players/2" ).push().set( data, callback_empty );
+				} );
+			}
+			break;
+		case "ice":
+			if( action.from == "1" || action.from == "2" ) {
+				game_rtc.addIceCandidate( new RTCIceCandidate( JSON.parse( action.ice ) ) );
+			}
+			break;
+		case "offer":
+			if( action.from == "1" ) {
+				game_rtc.setRemoteDescription( new RTCSessionDescription( JSON.parse( action.offer ) ) );
+				game_rtc.createAnswer().then( function( answer ) {
+					game_rtc.setLocalDescription( answer );
+				} ).then( function() {
+					var data = {};
+					data[ "type" ] = "answer";
+					data[ "from" ] = me;
+					data[ "answer" ] = JSON.stringify( game_rtc.localDescription );
+					data[ "id" ] = rtc_id++;
+					firebase.database().ref( duel + "/players/2" ).push().set( data, callback_empty );
+				} );
+			}
+			break;
+		case "answer":
+			if( action.from == "2" ) {
+				game_rtc.setRemoteDescription( new RTCSessionDescription( JSON.parse( action.answer ) ) );
+			}
+			break;
+	}
+}
+
+function callback_signals( snapshot ) {
+	if( !snapshot.exists() ) return;
+	snapshot.forEach( callback_rtc );
+}
+
+function callback_ice_candidate( evt ) {
+	if( evt.candidate ) {
+		var data = {},
+			id,
+			to;
+		if( this == game_rtc ) {
+			to = one ? "2" : "1";
+			to = "/players/" + to;
+			id = rtc_id++;
+		} else {
+			to = viewer_map[ this ];
+			to = "/viewers/" + to;
+		}
+		data[ "type" ] = "ice";
+		data[ "from" ] = me;
+		data[ "ice" ] = JSON.stringify( evt.candidate );
+		data[ "id" ] = id;
+		firebase.database().ref( duel + to ).push().set( data, callback_empty );
+	}
+}
+
+function callback_join_room( err ) {
+	if( err ) {
+
+	} else {
+		game_rtc = new RTCPeerConnection( RTC_CFG, RTC_OPT );
+		game_rtc.onicecandidate = callback_ice_candidate;
+		game_channel = game_rtc.createDataChannel( "game" );
+		firebase.database().ref( duel + "/players/1" ).on( "value", callback_signals );
+		firebase.database().ref( duel + "/players/2" ).on( "value", callback_signals );
+	}
+}
+
+function callback_query_room( snapshot ) {
+	if( snapshot.exists() ) {
+		if( snapshot.child( "active" ).val() ) {
+			var p = null,
+				data = null;
+			if( snapshot.child( "players" ).exists() ) {
+				if( snapshot.child( "players/2/" ).exists() ) {
+
+				} else {
+					me = p = "2";
+					one = false;
+				}
+			} else {
+				me = p = "1";
+			}
+			if( p != null ) {
+				data = {};
+				data[ "from" ] = me;
+				data[ "type" ] = "join";
+				data[ "id" ] = rtc_id++;
+				firebase.database().ref( duel + "/players/" + p ).push().set( data, callback_join_room );
+			}
+		}
+	} else {
+
+	}
+}
+
+firebase.database().ref( duel ).once( "value" ).then( callback_query_room );
 
 console.log( room_code );
--- docs/js/room_api.js
+++ docs/js/room_api.js
@@ -1,12 +1,43 @@
 // room_api.js
-function create_duel() {
+var ALPHABET = "ABCDEFGHIJKLMNOPQRSTUVWXYZ",
+	CODE_LENGTH = 5;
 
+var room_code = null;
+
+function random_code() {
+	var s = "";
+	for( var i = 0; i < CODE_LENGTH; ++i ) {
+		s += ALPHABET.charAt( ( Math.random() * 26 ) | 0 );
+	}
+	return s;
 }
 
-function join_duel() {
+function callback_set_room( err ) {
+	if( err ) {
 
+	} else {
+		window.location.replace( "../duel/?room=" + room_code );
+	}
+}
+
+function callback_query_room( snapshot ) {
+	if( snapshot.exists() ) {
+		firebase.database().ref( "/duels/" + ( room_code = random_code() ) ).once( "value" ).then( callback_query_room );
+	} else {
+		var data = {};
+		data[ "active" ] = true;
+		firebase.database().ref( "/duels/" + room_code ).set( data, callback_set_room );
+	}
+}
+
+function create_duel() {
+	firebase.database().ref( "/duels/" + ( room_code = random_code() ) ).once( "value" ).then( callback_query_room );
+}
+
+function join_duel( c ) {
+	window.location.replace( "../duel/?room=" + c );
 }
 
 function watch_duel() {
-	
+
 }
