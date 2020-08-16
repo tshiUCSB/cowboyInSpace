5b5851feb673cadfc29fc81ce626f08d3b9a2038
Tina Shi <tshi@umail.ucsb.edu>
2020-08-16 00:41:55 -0700

    added new assets and buttons to index.html

--- docs/js/index.js
+++ docs/js/index.js
@@ -1,1 +1,14 @@
 // index.js
+var bgMusic;
+
+function showButtons() {
+	document.getElementById("startControls").style.display = "initial";
+	document.getElementById("clickMe").style.display = "none";
+	bgMusic.play();
+}
+
+window.addEventListener('load', function() {
+	bgMusic = new Audio("assets/audio/Western_Spaghetti_-_Chris_Haugen.mp3");
+	bgMusic.loop = true;
+	document.getElementById("clickMe").addEventListener('click', showButtons, {'once': true});
+});
