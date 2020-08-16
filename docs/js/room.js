4390c77131c483fb47b95f6c2ddc5e70e306003b
Jared Flores <jared_flores@cs.ucsb.edu>
2020-08-16 00:45:22 -0700

    added some duel creating

--- docs/js/room.js
+++ docs/js/room.js
@@ -1,1 +1,19 @@
 // room.js
+function init_room() {
+	var isMobile = 
+		/Android|webOS|iPhone|iPad|iPod|BlackBerry|IEMobile|Opera Mini/i.test(navigator.userAgent) ?
+	 		true : false;
+
+	var BUTTON_CREATE = document.getElementById("createButton");
+
+	var click_evt = isMobile ? "touchend" : "click";
+
+	function handle_button_create() {
+		BUTTON_CREATE.removeEventListener( click_evt, handle_button_create );
+		create_duel();
+	}
+
+	BUTTON_CREATE.addEventListener( click_evt, handle_button_create );
+}
+
+window.addEventListener( "load", init_room() );
