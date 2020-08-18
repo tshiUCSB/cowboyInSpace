6c694ec79da9de8ddcd9d7a3bbd72d2e07b70c8e
Jared Flores <jared_flores@cs.ucsb.edu>
2020-08-18 04:03:59 -0700

    testing evt target

--- docs/js/duel.js
+++ docs/js/duel.js
@@ -285,14 +285,14 @@ function init_rtc( cb_init_rtc, cb_packet, cb_sync ) {
 			var data = {},
 				id,
 				to;
-			/*if( this == game_rtc ) {*/
+			if( evt.target == game_rtc ) {
 				to = one ? "2" : "1";
 				to = "/players/" + to;
 				id = rtc_id++;
-			/*} else {
-				to = viewer_map[ this ];
+			} else {
+				to = viewer_map[ evt.target ];
 				to = "/viewers/" + to;
-			}*/
+			}
 			data[ "type" ] = "ice";
 			data[ "from" ] = me;
 			data[ "ice" ] = JSON.stringify( evt.candidate );
